8713d0a7c66dec2fbb631a254832e60f39e77bd7
Karmen Beers <Karmenbeers@gmail.com>
2021-10-07 20:27:08 -0500

    capstone

--- store/Footer.js
+++
@@ -1,4 +0,0 @@
-export default () => `
-<footer>
-    &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
-  </footer>`;
--- store/Main.js
+++
@@ -1,4 +0,0 @@
-export default {
-  header: "Main",
-  view: "Main"
-};
