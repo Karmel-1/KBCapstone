bffd9e5efd43cefcb8d71b45c7d97e068f2b71ae
Karmen Beers <Karmenbeers@gmail.com>
2021-10-26 19:18:35 -0500

    update

--- store/Links.js
+++ store/Links.js
@@ -1,5 +1,5 @@
 export default [
+  { title: "Home", text: "Home" },
   { title: "About", text: "About" },
-  { title: "Contact", text: "Contact" },
-  { title: "Home", text: "Home" }
+  { title: "Contact", text: "Contact" }
 ];
